d3019fb8e240273431cb623043f53d51cbac9050
shaff khan <shaff.khan@techloyce.com>
2023-09-04 14:58:43 -0400

    home page creation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+//our styled components:
+const Container = styled.div`
+  background-color: black;
+  height: 65px;
+`;
+const Navbar = () => {
+  return <Container>navbar</Container>;
+};
+
+export default Navbar;
