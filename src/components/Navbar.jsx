755dda4b290729a95df8175dca94204834367b63
shaff khan <shaff.khan@techloyce.com>
2023-09-05 14:12:35 -0400

    home page creation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,29 @@ import styled from "styled-components";
 
 //our styled components:
 const Container = styled.div`
-  background-color: black;
+  ${"" /* background-color: black; */}
   height: 65px;
 `;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div``;
+const Right = styled.div``;
+const Center = styled.div``;
 const Navbar = () => {
-  return <Container>navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>Left</Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
