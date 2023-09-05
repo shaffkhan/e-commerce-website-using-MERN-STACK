ced337ce383ef59326246a8ca4856b93bfc10130
shaff khan <shaff.khan@techloyce.com>
2023-09-05 15:26:00 -0400

    home page creation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-
+import { Badge } from "@material-ui/core";
+import { Search,ShoppingCartOutlined } from "@material-ui/icons";
 //our styled components:
 const Container = styled.div`
-  ${"" /* background-color: black; */}
   height: 65px;
 `;
 
@@ -13,16 +13,66 @@ const Wrapper = styled.div`
   justify-content: space-between;
 `;
 
-const Left = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Right = styled.div`
+  display: flex;
+  align-items: center;
+  gap:25px;
+`;
 const Center = styled.div``;
+//left side components///
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+  margin-right: 25px;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid grey;
+  display: flex;
+  align-items: center;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+//Center components///
+const Logo = styled.h1`
+  font-weight: bold;
+  justify-content: center;
+  align-items: center;
+`;
+
+//right side components//
+const MenuItems = styled.div`
+  cursor: pointer;
+  font-weight: 14px;
+`;
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>Left</Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input /> <Search style ={{fontSize:20,color:"gray"}} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>Shaff.</Logo>
+        </Center>
+        <Right>
+          <MenuItems>Register</MenuItems>
+          <MenuItems>Sign In</MenuItems>
+          <Badge badgeContent={4} color="primary">
+            <ShoppingCartOutlined />
+          </Badge>
+        </Right>
       </Wrapper>
     </Container>
   );
